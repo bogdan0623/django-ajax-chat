d8680fe4eeb467f5c3705e45f53a58db8c8caf20
Bogdan Turdean <73332243+bogdan0623@users.noreply.github.com>
2023-10-29 12:20:44 +0200

    Add files via upload

---
+++ chat/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from chat import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+urlpatterns = [
+    path('', views.homepage, name="home"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('register/', views.register, name="register"),
+    path('<int:pk>/', views.chatroom, name="chatroom"),
+    path("ajax/<int:pk>/", views.ajax_load_messages, name="chatroom-ajax"),
+]
